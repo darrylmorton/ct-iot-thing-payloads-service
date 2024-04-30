d47366c491a3fd3931c8bb93871ea20c209068bf
darrylmorton <darryl.j.morton@gmail.com>
2024-04-30 17:07:21 +0100

    scripts

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,7 +40,7 @@ async def db_cleanup():
 
 
 @pytest.fixture
-async def seed_args() -> (int, datetime, datetime):
+async def seed_default_args() -> (int, datetime, datetime):
     payloads_total = 48
     end_timestamp = create_timestamp("2020-08-08T00:00:00Z")
     start_timestamp = end_timestamp - datetime.timedelta(days=payloads_total)
@@ -49,8 +49,8 @@ async def seed_args() -> (int, datetime, datetime):
 
 
 @pytest.fixture
-async def thing_payloads_fixture(db_cleanup, seed_args) -> (datetime, datetime):
-    payloads_total, start_timestamp, end_timestamp = seed_args
+async def thing_payloads_fixture(db_cleanup, seed_default_args) -> (datetime, datetime):
+    payloads_total, start_timestamp, end_timestamp = seed_default_args
 
     payloads = thing_payloads_seed(payloads_total, start_timestamp)
 
