1fc572eed6b7d3643770ea17b09c4a779fa71bee
darrylmorton <darryl.j.morton@gmail.com>
2024-08-13 23:09:15 +0100

    cleanup

--- tests/integration/routers/healthz_route_test.py
+++ tests/integration/routers/healthz_route_test.py
@@ -1,11 +1,12 @@
 from config import APP_VERSION
 from tests.helper.routes_helper import RoutesHelper
+from thing_payloads_service.service import server
 
 
-async def test_health():
+async def test_healthz():
     expected_result = {"message": "ok", "version": APP_VERSION}
 
-    response = await RoutesHelper.http_client(RoutesHelper.TEST_URL, "healthz")
+    response = await RoutesHelper.http_client(server, RoutesHelper.TEST_URL, "/healthz")
 
     assert response.status_code == 200
     assert response.json() == expected_result
